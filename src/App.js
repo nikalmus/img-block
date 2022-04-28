926e12bfc009f43cd8e1cc5c6bcf2b2f201e61db
nickrally <nickrally2009@gmail.com>
2022-04-28 09:05:06 -0600

    dnd without replacement

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
-import React, { useMemo } from "react";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import React, { useState, useMemo } from "react";
+import { useDrop } from "react-dnd";
 import Image from "./Image";
 import ImageBlock from "./ImageBlock";
 import paintings from "./img";
 
 function App() {
-  const images = useMemo(() => [
-    {
-      id: 1,
-      src: paintings.cavePainting,
-    },
-    {
-      id: 2,
-      src: paintings.lasMeninasVelazquez,
-    },
-    {
-      id: 3,
-      src: paintings.daughtersOfSargent,
-    },
-    {
-      id: 4,
-      src: paintings.lasMeninasPicasso,
-    },
-  ]);
+  const images = useMemo(
+    () => [
+      {
+        id: 1,
+        src: paintings.cavePainting,
+      },
+      {
+        id: 2,
+        src: paintings.lasMeninasVelazquez,
+      },
+      {
+        id: 3,
+        src: paintings.daughtersOfSargent,
+      },
+      {
+        id: 4,
+        src: paintings.lasMeninasPicasso,
+      },
+    ],
+    []
+  );
+  const [blockImagesList, setBlockImagesList] = useState([]);
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "picture",
+    drop: (item) => addImageToBlock(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addImageToBlock = (id) => {
+    console.log("id:", id);
+    const blockImage = images.filter((img) => id === img.id)[0];
+    setBlockImagesList((blockImagesList) => [...blockImagesList, blockImage]);
+  };
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="main">
-        <div className="repo">
-          {images.map((image) => (
-            <Image key={image.id} img={image.src} id={image.id} />
-          ))}
-        </div>
-        <div>
-          {images.map((image) => (
-            <ImageBlock key={image.id} img={image.src} />
-          ))}
-        </div>
+    <div className="main">
+      <div className="repo">
+        {images.map((image) => (
+          <Image key={image.id} img={image.src} id={image.id} />
+        ))}
       </div>
-    </DndProvider>
+      <div ref={drop} className="drop">
+        {blockImagesList.map((image) => (
+          <ImageBlock key={image.id} img={image.src} />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/Image.js
+++ src/Image.js
@@ -4,6 +4,7 @@ import { useDrag } from "react-dnd";
 const Image = ({ img, id }) => {
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "picture",
+    item: { id: id },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
